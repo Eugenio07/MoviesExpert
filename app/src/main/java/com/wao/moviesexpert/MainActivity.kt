955ea9fdacdf4c1ed785e26b5e5ea36036b7fb42
Eugenio <eormartinez20@gmail.com>
2022-12-12 11:08:24 -0400

    Se crea estilo para media item

--- app/src/main/java/com/wao/moviesexpert/MainActivity.kt
+++ app/src/main/java/com/wao/moviesexpert/MainActivity.kt
@@ -3,13 +3,17 @@ package com.wao.moviesexpert
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wao.moviesexpert.ui.theme.MoviesExpertTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,15 +33,44 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun MediaItem() {
+    Column {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+                .background(color = Color.Red)
+        ) {
+
+        }
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colors.secondary)
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Title 1",
+                style = MaterialTheme.typography.h6
+            )
+        }
+    }
+}
+
+
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+/*
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MoviesExpertTheme {
         Greeting("Android")
     }
-}
+}*/
