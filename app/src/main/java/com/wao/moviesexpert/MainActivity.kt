a4087f4a542b03261da2d452888e1107a9944e02
Eugenio <eormartinez20@gmail.com>
2022-12-12 11:27:33 -0400

    Se agregan imports

--- app/src/main/java/com/wao/moviesexpert/MainActivity.kt
+++ app/src/main/java/com/wao/moviesexpert/MainActivity.kt
@@ -5,15 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayCircleOutline
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.wao.moviesexpert.ui.theme.MoviesExpertTheme
 
 class MainActivity : ComponentActivity() {
